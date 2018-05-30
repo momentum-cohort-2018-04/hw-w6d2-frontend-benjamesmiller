ba8bd4d9b1a6d5185cfd6c4debed4110ea2c3c90
benjamesmiller <benjamesmiller@gmail.com>
2018-05-30 14:37:38 -0400

    reworking render

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import books from './books.json';
 
 class App extends Component {
+  constructor () {
+    super() ;
+    this.state = {
+      books: books,
+    }
+  }
   render() {
+    const books = this.state.books;
+    const author = this.state.books.author;
+    const title = this.state.books.title;
+    const pic = this.state.books.coverImageUrl;
+    const short = this.state.books.shortdescription;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro"> 
+        <div className="generalInfo">
+        {books.map((books, idx) =>
+        <div key={idx} className="title">
+        {books.title}
+        </div>
+        <div key={idx} className="author">
+        {books.author}
+        </div>
+        <div key={idx} className="short">
+        {books.shortdescription}
+        </div>
+        <div key={idx} className="pic">
+        {books.coverImageUrl}
+        </div>
+        )}
+        </div>
         </p>
       </div>
     );
